17a8b1909416e2c0c15d1d436da815e2a5ffd964
longya <longya1@jushuitan.com>
2024-11-06 10:56:24 +0800

    1

--- codes/perspective_transformation.py
+++ codes/perspective_transformation.py
@@ -5,12 +5,12 @@ from utils.cv_util import show_image
 src_image = cv2.imread('../images/saber.jpg')
 dst_image = cv2.imread('../images/test.jpeg')
 # show_image('saber', src_image)
-# show_image('test', dst_image)
+show_image('test', dst_image)
 # 目标图像中不规则四边形顶点
-dst_x0, dst_y0 = 48, 47
-dst_x1, dst_y1 = 300, 93
-dst_x2, dst_y2 = 321, 266
-dst_x3, dst_y3 = 35, 253
+dst_x0, dst_y0 = 59, 65
+dst_x1, dst_y1 = 289, 107
+dst_x2, dst_y2 = 305, 257
+dst_x3, dst_y3 = 51, 244
 dst_points = np.array([[dst_x0, dst_y0], [dst_x1, dst_y1], [dst_x2, dst_y2], [dst_x3, dst_y3]], dtype=np.float32)
 dst_points = dst_points.reshape((-1, 1, 2))
 # 根据透视变换的目标区域将图片resize到合适大小，否则透视变换后清晰度会大幅降低，经过比较INTER_AREA插值法最优
@@ -24,10 +24,10 @@ M = cv2.getPerspectiveTransform(src_rect, dst_points)
 transformed_image = cv2.warpPerspective(src_image, M, (
     int(np.linalg.norm(dst_points[0] - dst_points[2])), int(np.linalg.norm(dst_points[1] - dst_points[3]))))
 show_image('transformed_image', transformed_image)
-# 目标区域填充黑色
+# 目标图片四边形区域填黑色 重点:直线抗锯齿 cv2.LINE_AA
 dst_image_copy = dst_image.copy()
 dst_points = dst_points.astype(np.int32)
-cv2.fillPoly(dst_image_copy, [dst_points], (0, 0, 0), lineType=cv2.LINE_8)
+cv2.fillPoly(dst_image_copy, [dst_points], (0, 0, 0), lineType=cv2.LINE_AA)
 show_image('dst_image_copy', dst_image_copy)
 
 # 转换图片加embedding
